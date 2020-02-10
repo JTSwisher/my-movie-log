334d5eac380f5bc6405cdfeb2bcb397d2315d8a4
JTSwisher <jefftswisher@gmail.com>
2020-02-10 11:24:44 -0700

    Add forms to signup, login & failure page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,17 @@
 class UserController < ApplicationController 
 
-    get '/signup' do 
+    get "/signup" do 
         erb :'/users/signup'
     end 
 
-    post '/signup' do 
-
+    post "/signup" do 
+        binding.pry
+        user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if user.save
+            redirect "/login"
+        else 
+            redirect "/signup"
+        end 
     end 
 
     get '/login' do 
@@ -13,8 +19,45 @@ class UserController < ApplicationController
     end 
 
     post '/login' do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
+    end 
+
+    get '/success' do 
+        if logged_in?
+            erb :success
+        else 
+            redirect "/login"
+        end 
+    end 
+
+    get '/failure' do 
+        erb :'users/failure'
+    end 
+
+    get "/logout" do
+		session.clear
+		redirect "/"
+	end
+
+    helpers do 
+        
+        def logged_in?
+            !!session[:user_id]
+        end 
+
+        def current_user 
+            User.find(session[:user_id])
+        end 
 
     end 
+
+
 
 
 end 
