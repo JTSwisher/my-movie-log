d5850000ccf2a4bf6c95e2291f45c28371b2dca0
JTSwisher <jefftswisher@gmail.com>
2020-02-10 09:25:27 -0700

    Create link to login page from sign in button

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,17 @@ class UserController < ApplicationController
         erb :'/users/signup'
     end 
 
+    post '/signup' do 
+
+    end 
+
+    get '/login' do 
+        erb :'/users/login'
+    end 
+
+    post '/login' do 
+
+    end 
+
+
 end 
