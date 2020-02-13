20d10e03579edd11d1522e160de953d8b71f70ab
JTSwisher <jefftswisher@gmail.com>
2020-02-12 21:02:37 -0700

    Remove sqlite, add postgres for heroku deploy

--- db/migrate/20200213015737_add_poster_column.rb
+++ db/migrate/20200213015737_add_poster_column.rb
@@ -1,4 +1,4 @@
-class AddPosterColumn < ActiveRecord::Migration
+class AddPosterColumn < ActiveRecord::Migration[4.2]
   def change
     add_column :movies, :poster, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,28 +1,30 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200213015737) do
+ActiveRecord::Schema.define(version: 2020_02_13_015737) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string  "title"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "movies", id: :serial, force: :cascade do |t|
+    t.string "title"
     t.integer "rating"
-    t.text    "comments"
-    t.date    "date"
+    t.text "comments"
+    t.date "date"
     t.integer "user_id"
-    t.string  "poster"
+    t.string "poster"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
